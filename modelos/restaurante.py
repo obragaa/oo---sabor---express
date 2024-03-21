18d07387d0635f0f0b89054038410a1395a4798c
Felipe Braga Duarte <f.braga.duarte02@gmail.com>
2024-03-21 10:18:07 -0300

    Aula 02

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,18 +1,27 @@
+# print(dir(restaurante_praca)) --> Atributos, métodos e propriedades
+# print(vars(restaurante_praca)) --> Dicionário dos Atributos e métodos
+# self --> Referência da instância naquele momento
+# __init__ --> Método construtor
+# __str__ --> Método que pega o objeto e mostra em um formato de texto definido por nós
+
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
+
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
+
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
 
 # Objetos da Classe
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurante_pizza = Restaurante()
-
-restaurantes = [restaurante_pizza, restaurante_praca]
-
-# print(dir(restaurante_praca)) --> Atributod, métodos e propriedades
-# print(vars(restaurante_praca)) --> Dicionário dos Atributos e métodos
-
-print(vars(restaurante_praca))
+Restaurante.listar_restaurantes()
